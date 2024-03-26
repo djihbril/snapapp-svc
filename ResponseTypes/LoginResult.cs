f01a32a17e53598da63e13ecee9b0386d78c7685
Djihbrihou Abibou <djihbril@outlook.com>
2024-03-26 15:16:40 -0400

    Adding `LoginResult` response type

---
+++ ResponseTypes/LoginResult.cs
@@ -0,0 +1,13 @@
+using Microsoft.Azure.Functions.Worker.Extensions.Sql;
+using Microsoft.Azure.Functions.Worker.Http;
+using SnapApp.Svc.DbModels;
+
+namespace SnapApp.Svc.ResponseTypes
+{
+    public class LoginResult
+    {
+        [SqlOutput("dbo.Logins", connectionStringSetting: "SqlConnectionString")]
+        public Login? Login { get; set; }
+        public required HttpResponseData HttpResponse { get; set; }
+    }
+}

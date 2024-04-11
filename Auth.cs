c28fc75a51d55c2da411f369d81d944a40d19fb5
Djihbrihou Abibou <djihbril@outlook.com>
2024-04-11 16:01:10 -0400

    Adding various tweaks to the Auth class:
    * switching to new namespace syntax
    * renaming `logins` to `loginInfos`
    * renaming `newLogin` to `login`
    * returning user id with the user data on successful login
    * using standard empty string construct
    * returning user id with successful signup payload

--- Auth.cs
+++ Auth.cs
@@ -12,191 +12,191 @@ using SnapApp.Svc.Models;
 using SnapApp.Svc.ResponseTypes;
 using FromBodyAttribute = Microsoft.Azure.Functions.Worker.Http.FromBodyAttribute;
 
-namespace SnapApp.Svc
+namespace SnapApp.Svc;
+
+public class Auth(ILogger<Auth> logger)
 {
-    public class Auth(ILogger<Auth> logger)
+    [Function("Login")]
+    public async Task<LoginResult> RunLogin([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req, [FromBody] Credentials creds,
+        [SqlInput(commandText: "GetLoginInfo", commandType: System.Data.CommandType.StoredProcedure, parameters: "@email={Username}",
+        connectionStringSetting: "SqlConnectionString")] IEnumerable<LoginInfo> loginInfos)
     {
-        [Function("Login")]
-        public async Task<LoginResult> RunLogin([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req, [FromBody] Credentials creds,
-            [SqlInput(commandText: "GetLoginInfo", commandType: System.Data.CommandType.StoredProcedure, parameters: "@email={Username}",
-            connectionStringSetting: "SqlConnectionString")] IEnumerable<LoginInfo> logins)
+        logger.LogInformation("[Login] function processed a request.");
+
+        HttpResponseData resp = req.CreateResponse();
+
+        async Task<LoginResult> DenyAuth()
         {
-            logger.LogInformation("[Login] function processed a request.");
+            resp.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            resp.StatusCode = HttpStatusCode.Unauthorized;
+            await resp.WriteStringAsync("Invalid username or password.");
 
-            HttpResponseData resp = req.CreateResponse();
-
-            async Task<LoginResult> DenyAuth()
+            return new LoginResult
             {
-                resp.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-                resp.StatusCode = HttpStatusCode.Unauthorized;
-                await resp.WriteStringAsync("Invalid username or password.");
+                HttpResponse = resp
+            };
+        }
 
-                return new LoginResult
-                {
-                    HttpResponse = resp
-                };
-            }
+        if (loginInfos == null || !loginInfos.Any())
+        {
+            return await DenyAuth();
+        }
 
-            if (logins == null || !logins.Any())
+        LoginInfo loginInfo = loginInfos.First();
+
+        if (creds.Password.HashPassword(loginInfo.Salt) != loginInfo.UserPassword)
+        {
+            return await DenyAuth();
+        }
+
+        DateTime now = DateTime.UtcNow;
+
+        Login login = new()
+        {
+            UserId = loginInfo.UserId,
+            CryptoKeys = loginInfo.CryptoKeys,
+            ExpiresOn = now.AddSeconds(Settings.TokenExpirationSpanInSecs),
+            CreatedOn = now
+        };
+
+        AccessToken accessToken = new()
+        {
+            UserId = loginInfo.UserId,
+            Role = loginInfo.UserRole,
+            IssuedOn = now
+        };
+
+        if (loginInfo.LoginCreatedOn != null)
+        {
+            // Set the new login Id with the existing login id so that the login recorded is updated.
+            login.Id = loginInfo.Id;
+        }
+        else
+        {
+            login.CryptoKeys = new RSACryptoServiceProvider(2048).ExportCspBlob(true);
+        }
+
+        using RSACryptoServiceProvider rsa = new();
+
+        rsa.ImportCspBlob(login.CryptoKeys);
+        string tokenJson = JsonSerializer.Serialize(accessToken, JsonSerializationOptions.CamelCaseNamingOptions);
+        string token = Convert.ToBase64String(rsa.Encrypt(Encoding.UTF8.GetBytes(tokenJson), false));
+
+        resp.Headers.Add("Content-Type", "application/json; charset=utf-8");
+        resp.StatusCode = HttpStatusCode.OK;
+        await resp.WriteStringAsync(JsonSerializer.Serialize(new
+        {
+            UserInfo = new
             {
-                return await DenyAuth();
-            }
+                Id = loginInfo.UserId,
+                Email = loginInfo.UserEmail,
+                Company = loginInfo.UserCompany,
+                FirstName = loginInfo.UserFirstName,
+                LastName = loginInfo.UserLastName,
+                Phone = loginInfo.UserPhone,
+                Role = loginInfo.UserRole,
+                Picture = loginInfo.UserPicture,
+                IsEmailVerified = loginInfo.IsUserEmailVerified,
+                CreatedOn = loginInfo.UserCreatedOn
+            },
+            AccessToken = token
+        }, JsonSerializationOptions.CamelCaseNamingOptions));
 
-            LoginInfo login = logins.First();
+        return new LoginResult
+        {
+            Login = login,
+            HttpResponse = resp
+        };
+    }
 
-            if (creds.Password.HashPassword(login.Salt) != login.UserPassword)
+    [Function("SignUp")]
+    public async Task<SignUpResult> RunSignUp([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req, [FromBody] SignUp signUp,
+        [SqlInput(commandText: "CheckExistingUser", commandType: System.Data.CommandType.StoredProcedure, parameters: "@email={Email}",
+        connectionStringSetting: "SqlConnectionString")] string scalarJson)
+    {
+        logger.LogInformation("[SignUp] function processed a request.");
+
+        var scalarObj = JsonSerializer.Deserialize<object>(scalarJson);
+        bool userExists = scalarObj is JsonElement jEle && jEle[0].GetProperty("UserExists").GetInt32() != 0;
+        HttpResponseData resp = req.CreateResponse();
+
+        if (userExists)
+        {
+            resp.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            resp.StatusCode = HttpStatusCode.Conflict;
+            await resp.WriteStringAsync($"User {signUp.Email} already exists.");
+
+            return new SignUpResult()
             {
-                return await DenyAuth();
-            }
+                HttpResponse = resp
+            };
+        }
 
+        if (signUp != null)
+        {
             DateTime now = DateTime.UtcNow;
+            Guid userId = Guid.NewGuid();
 
-            Login newLogin = new()
+            User user = new()
             {
-                UserId = login.UserId,
-                CryptoKeys = login.CryptoKeys,
-                ExpiresOn = now.AddSeconds(Settings.TokenExpirationSpanInSecs),
+                Id = userId,
+                Email = signUp.Email,
+                Password = string.Empty,
+                Company = signUp.Company,
+                FirstName = signUp.FirstName,
+                LastName = signUp.LastName,
+                Phone = signUp.Phone,
+                Role = signUp.Role,
+                Salt = [],
                 CreatedOn = now
             };
 
-            AccessToken accessToken = new()
+            string tokenJson = JsonSerializer.Serialize(new AccessToken
             {
-                UserId = login.UserId,
-                Role = login.UserRole,
+                UserId = userId,
+                Role = signUp.Role,
                 IssuedOn = now
-            };
+            }, JsonSerializationOptions.CamelCaseNamingOptions);
 
-            if (login.LoginCreatedOn > DateTime.MinValue)
-            {
-                newLogin.Id = login.Id;
-                newLogin.CreatedOn = login.LoginCreatedOn;
-                accessToken.IssuedOn = login.LoginCreatedOn;
-            }
-            else
-            {
-                newLogin.CryptoKeys = new RSACryptoServiceProvider(2048).ExportCspBlob(true);
-            }
+            using RSACryptoServiceProvider rsa = new(2048);
 
-            using RSACryptoServiceProvider rsa = new();
-            rsa.ImportCspBlob(newLogin.CryptoKeys);
+            user.Salt = CryptoHelpers.GenerateSalt();
 
-            string tokenJson = JsonSerializer.Serialize(accessToken, JsonSerializationOptions.CamelCaseNamingOptions);
+            user.Password = signUp.Password.HashPassword(user.Salt);
             string token = Convert.ToBase64String(rsa.Encrypt(Encoding.UTF8.GetBytes(tokenJson), false));
 
             resp.Headers.Add("Content-Type", "application/json; charset=utf-8");
             resp.StatusCode = HttpStatusCode.OK;
             await resp.WriteStringAsync(JsonSerializer.Serialize(new
             {
-                UserInfo = new
-                {
-                    Email = login.UserEmail,
-                    Company = login.UserCompany,
-                    FirstName = login.UserFirstName,
-                    LastName = login.UserLastName,
-                    Phone = login.UserPhone,
-                    Role = login.UserRole,
-                    Picture = login.UserPicture,
-                    IsEmailVerified = login.IsUserEmailVerified,
-                    CreatedOn = login.UserCreatedOn
-                },
+                UserId = userId,
+                UserCreatedOn = now,
                 AccessToken = token
             }, JsonSerializationOptions.CamelCaseNamingOptions));
 
-            return new LoginResult
+            return new SignUpResult()
             {
-                Login = newLogin,
+                User = user,
+                Login = new()
+                {
+                    UserId = userId,
+                    CryptoKeys = rsa.ExportCspBlob(true),
+                    ExpiresOn = now.AddSeconds(Settings.TokenExpirationSpanInSecs),
+                    CreatedOn = now
+                },
                 HttpResponse = resp
             };
         }
-
-        [Function("SignUp")]
-        public async Task<SignUpResult> RunSignUp([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req, [FromBody] SignUp signUp,
-            [SqlInput(commandText: "CheckExistingUser", commandType: System.Data.CommandType.StoredProcedure, parameters: "@email={Email}",
-            connectionStringSetting: "SqlConnectionString")] string scalarJson)
+        else
         {
-            logger.LogInformation("[SignUp] function processed a request.");
+            resp.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            resp.StatusCode = HttpStatusCode.NotFound;
+            await resp.WriteStringAsync("Missing signup request content.");
 
-            var scalarObj = JsonSerializer.Deserialize<object>(scalarJson);
-            bool userExists = scalarObj is JsonElement jEle && jEle[0].GetProperty("UserExists").GetInt32() != 0;
-            HttpResponseData resp = req.CreateResponse();
-
-            if (userExists)
+            return new SignUpResult()
             {
-                resp.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-                resp.StatusCode = HttpStatusCode.Conflict;
-                await resp.WriteStringAsync($"User {signUp.Email} already exists.");
-
-                return new SignUpResult()
-                {
-                    HttpResponse = resp
-                };
-            }
-
-            if (signUp != null)
-            {
-                DateTime now = DateTime.UtcNow;
-                Guid userId = Guid.NewGuid();
-
-                User user = new()
-                {
-                    Id = userId,
-                    Email = signUp.Email,
-                    Password = "",
-                    Company = signUp.Company,
-                    FirstName = signUp.FirstName,
-                    LastName = signUp.LastName,
-                    Phone = signUp.Phone,
-                    Role = signUp.Role,
-                    Salt = [],
-                    CreatedOn = now
-                };
-
-                string tokenJson = JsonSerializer.Serialize(new AccessToken
-                {
-                    UserId = userId,
-                    Role = signUp.Role,
-                    IssuedOn = now
-                }, JsonSerializationOptions.CamelCaseNamingOptions);
-
-                using RSACryptoServiceProvider rsa = new(2048);
-
-                user.Salt = CryptoHelpers.GenerateSalt();
-
-                user.Password = signUp.Password.HashPassword(user.Salt);
-                string token = Convert.ToBase64String(rsa.Encrypt(Encoding.UTF8.GetBytes(tokenJson), false));
-
-                resp.Headers.Add("Content-Type", "application/json; charset=utf-8");
-                resp.StatusCode = HttpStatusCode.OK;
-                await resp.WriteStringAsync(JsonSerializer.Serialize(new
-                {
-                    UserCreatedOn = now,
-                    AccessToken = token
-                }, JsonSerializationOptions.CamelCaseNamingOptions));
-
-                return new SignUpResult()
-                {
-                    User = user,
-                    Login = new()
-                    {
-                        UserId = userId,
-                        CryptoKeys = rsa.ExportCspBlob(true),
-                        ExpiresOn = now.AddSeconds(Settings.TokenExpirationSpanInSecs),
-                        CreatedOn = now
-                    },
-                    HttpResponse = resp
-                };
-            }
-            else
-            {
-                resp.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-                resp.StatusCode = HttpStatusCode.NotFound;
-                await resp.WriteStringAsync("Missing signup request content.");
-
-                return new SignUpResult()
-                {
-                    HttpResponse = resp
-                };
-            }
+                HttpResponse = resp
+            };
         }
     }
 }

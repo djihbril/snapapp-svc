dcef8553cd920080cee7d4b56c50a69d04827348
Djihbrihou Abibou <djihbril@outlook.com>
2024-04-11 15:44:52 -0400

    Registering bearer auth middleware and related services
    * a SQL database connection
    * a database service

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using SnapApp.Svc.Middleware;
+using SnapApp.Svc.Extensions;
+using SnapApp.Svc.Services;
+using System.Data.Common;
+using Microsoft.Data.SqlClient;
+using SnapApp.Svc;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
+    .ConfigureFunctionsWebApplication(configure =>
+    {
+        configure.UseWhen<BearerAuthenticationMiddleware>(context =>
+        {
+            FunctionDefinition fnDef = context.FunctionDefinition;
+            FunctionAuthorizeAttribute? fnAttr = fnDef.GetFunctionAuthorizeAttribute();
+
+            return fnAttr != null && fnDef.InputBindings.Values.First(a => a.Type.EndsWith("Trigger")).Type == "httpTrigger";
+        });
+    })
     .ConfigureServices(services => {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+
+        services.AddScoped<DbConnection>(provider => new SqlConnection(Settings.SqlConnectionString));
+        services.AddScoped<IDatabaseService, DatabaseContext>();
     })
     .Build();
 
